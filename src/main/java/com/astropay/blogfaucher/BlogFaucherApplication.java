cda29109a5d2457b96c1a0fcc54b2c4d9d541c1d
faucher-agustin <agustinfaucher@gmail.com>
2022-09-22 17:54:53 -0300

    Initial scaffolding

---
+++ src/main/java/com/astropay/blogfaucher/BlogFaucherApplication.java
@@ -0,0 +1,13 @@
+package com.astropay.blogfaucher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogFaucherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogFaucherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/astropay/blogfaucher/BlogFaucherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astropay.blogfaucher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogFaucherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
